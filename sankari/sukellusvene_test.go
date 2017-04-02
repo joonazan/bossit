8abebbc9160b74d4ba86624c2d3fc03d9fb9a830
Joonatan Saarhelo <joon.saar@gmail.com>
2017-04-02 12:12:57 +0300

    parempi testi sukellusvenetehtävään.

--- sankari/sukellusvene_test.go
+++ sankari/sukellusvene_test.go
@@ -1,27 +1,26 @@
 package sankari
 
 import (
-	"math/rand"
 	"testing"
-	"time"
 )
 
 func TestUpotus(t *testing.T) {
-	löydetty := false
-	rand.Seed(time.Now().UnixNano())
-	paikka := rand.Intn(100) + 1
-	LöydäSukellusvene(func(arvaus int) int {
-		if arvaus < paikka {
-			return 1
-		} else if arvaus > paikka {
-			return -1
-		} else {
-			löydetty = true
-			return 0
-		}
-	})
 
-	if löydetty == false {
-		t.Fatal("Näillä etsinnöillä sukellusvenettä ei löytynyt.")
+	for paikka := 1; paikka <= 100; paikka++ {
+		löydetty := false
+		LöydäSukellusvene(func(arvaus int) int {
+			if arvaus < paikka {
+				return 1
+			} else if arvaus > paikka {
+				return -1
+			} else {
+				löydetty = true
+				return 0
+			}
+		})
+
+		if löydetty == false {
+			t.Fatalf("Näillä etsinnöillä sukellusvenettä ei löytyisi jos se olisi laivan %d alla.\n", paikka)
+		}
 	}
 }
